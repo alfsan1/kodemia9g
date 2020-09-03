bbdcc5defdf2db35bafad77f0e1926b8b47fcf96
alfsan1 <santosabascal@gmail.com>
2020-09-02 21:06:10 -0500

    actualización de clases

---
+++ 2ago2020/js/main.js
@@ -0,0 +1,24 @@
+
+// necesito saber si el usuario necesita sumar, miult o dividir
+const calculadora = operacion  => {
+    console.log(operacion)
+    
+    switch(operacion){
+        case  "suma" :
+            alert("vas a hacer una suma")
+            break
+        case  "resta" :
+                alert("vas a hacer una resta")
+                break
+
+        case  "division" :
+                    alert("vas a hacer una division")
+                    break
+         case  "multiplcacion" :
+                        alert("vas a hacer una multiplicación")
+                        break
+        
+    }
+
+}
+
